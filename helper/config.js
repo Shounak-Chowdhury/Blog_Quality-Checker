0901185ba7723bc3b1fd4779807f66450f774570
Shounak Chowdhury <shounakrishi555@gmail.com>
2022-08-01 01:11:20 +0530

    first commit

---
+++ helper/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURL: 'mongodb://User:shounak555@cluster0-shard-00-00.1obfu.mongodb.net:27017,cluster0-shard-00-01.1obfu.mongodb.net:27017,cluster0-shard-00-02.1obfu.mongodb.net:27017/analyse?ssl=true&replicaSet=atlas-p7ov2s-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
